2e65bac0dc1207342981d069b998d3b7c55aec94
lcrippa <crippa.lorenzo@gmail.com>
2019-09-27 17:06:15 +0200

    INTERMEDIATE COMMIT:
    The code compiles up to the driver.
    
    NOTES:
    ED_FIT_CHI2.f90   is only at the stage of not giving compiler errors,
                      needs debug, especially for the temp bath in the
                      function delta_replica
    SPIN-FLIP         implemented for sparse matrix case. Needs check
    CDMFT_ED.f90      maybe some functions need to be added

--- CDMFT_ED.f90
+++ CDMFT_ED.f90
@@ -8,58 +8,52 @@ MODULE CDMFT_ED
        nnn2lso_reshape                        , &
        so2nn_reshape                          , &
        nn2so_reshape                          , &
-       ed_search_variable                     , &
-       search_chemical_potential
+       ed_search_variable
 
 
   USE ED_IO,      only:                         &
        ed_print_impSigma                      , &
        ed_print_impG                          , &
        ed_print_impG0                         , &
-       ed_print_impChi                        , &
+       !ed_print_impChi                        , &
        ed_get_sigma_matsubara                 , &
-       ed_get_sigma_real                      , &
+       ed_get_sigma_realaxis                  , &
        ed_get_gimp_matsubara                  , &
-       ed_get_gimp_real                       , &
+       ed_get_gimp_realaxis                   , &
        ed_get_g0imp_matsubara                 , &
-       ed_get_g0imp_real                      , &
-       ed_get_dens                            , &
-       ed_get_mag                             , &
-       ed_get_docc                            , &
-       ed_get_eimp                            , &
-       ed_get_epot                            , &
-       ed_get_eint                            , &
-       ed_get_ehartree                        , &
-       ed_get_eknot                           , &
-       ed_get_doubles                         , &
-       ed_get_density_matrix                  , &
-       ed_read_impSigma_single                , &
-       ed_read_impSigma_lattice
+       ed_get_g0imp_realaxis                  !, &
+       !ed_get_dens                            , &
+       !ed_get_mag                             , &
+       !ed_get_docc                            , &
+       !ed_get_eimp                            , &
+       !ed_get_epot                            , &
+       !ed_get_eint                            , &
+       !ed_get_ehartree                        , &
+       !ed_get_eknot                           , &
+       !ed_get_doubles                         , &
+       !ed_get_density_matrix                  , &
+       !ed_read_impSigma_single                , &
+       !ed_read_impSigma_lattice
 
 
   USE ED_BATH, only:                            &
        get_bath_dimension                     , &
-       get_component_bath_dimension           , &
-       get_spin_component_bath_dimension      , &
-       get_orb_component_bath_dimension       , &
-       get_spin_orb_component_bath_dimension  , &
-       get_component_bath                     , &
-       set_component_bath                     , &      
-       copy_component_bath                    , &
        spin_symmetrize_bath                   , &
-       orb_symmetrize_bath                    , &
        orb_equality_bath                      , &
-       ph_symmetrize_bath                     , &
-       ph_trans_bath                          , &
+       !ph_symmetrize_bath                     , &
+       !ph_trans_bath                          , &
        break_symmetry_bath
 
   USE ED_BATH_FUNCTIONS, only:                  &
-       ed_delta_matsubara                     , &
-       ed_g0and_matsubara                     , &
-       ed_invg0_matsubara                     , &
-       ed_delta_realaxis                      , &
-       ed_g0and_realaxis                      , &
-       ed_invg0_realaxis
+       !ed_delta_matsubara                     , &
+       !ed_g0and_matsubara                     , &
+       !ed_invg0_matsubara                     , &
+       !ed_delta_realaxis                      , &
+       !ed_g0and_realaxis                      , &
+       !ed_invg0_realaxis
+       delta_bath                               ,&
+       g0and_bath                               ,&
+       invg0_bath
 
 
   USE ED_MAIN, only:                            &
